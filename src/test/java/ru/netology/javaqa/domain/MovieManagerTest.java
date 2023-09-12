6bae6c110eb99a464a0a329626f8b22c16c73a48
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-09-12 12:55:57 +0300

    correction

--- src/test/java/ru/netology/javaqa/domain/MovieManagerTest.java
+++ src/test/java/ru/netology/javaqa/domain/MovieManagerTest.java
@@ -17,6 +17,8 @@ public class MovieManagerTest {
 
     @Test
     public void shooldEqualDefaultLimit() {
+        MovieManager manager = new MovieManager();
+
 
         manager.add("Film 1");
         manager.add("Film 2");
@@ -29,7 +31,7 @@ public class MovieManagerTest {
         manager.add("Film 9");
         manager.add("Film 10");
 
-        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5", "Film 6", "Film 7", "Film 8", "Film 9", "Film 10"};
+        String[] expected = {"Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
